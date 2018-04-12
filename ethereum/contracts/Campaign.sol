f08913aa4a1c2df4c97b4758cacadd005406d171
Andrey <andrey@dpromo.su>
2018-04-12 06:06:41 +0300

    add mocha testing

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,17 +1,16 @@
 pragma solidity ^0.4.17;
 
 contract CampaignFactory {
-    address[] public deployContracts;
+    address[] public deployedCampaigns;
 
     function createCampaign(uint minimum) public {
         address newCampaign = new Campaign(minimum, msg.sender);
-        deployContracts.push(newCampaign);
+        deployedCampaigns.push(newCampaign);
     }
 
-    function getDeployCampaign() public view returns (address[]) {
-        return deployContracts;
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
     }
-
 }
 
 contract Campaign {
@@ -24,16 +23,16 @@ contract Campaign {
         mapping(address => bool) approvals;
     }
 
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-
-    Request[] public requests;
-    address public manager;
-    uint public minimumContribution;
-    mapping (address => bool) public approvers;
-    uint approversCount;
 
     function Campaign(uint minimum, address creator) public {
         manager = creator;
@@ -42,21 +41,21 @@ contract Campaign {
 
     function contribute() public payable {
         require(msg.value > minimumContribution);
+
         approvers[msg.sender] = true;
         approversCount++;
     }
 
-    function createRequest(string description, uint value, address recipient) public restricted{
+    function createRequest(string description, uint value, address recipient) public restricted {
         Request memory newRequest = Request({
-            description: description,
-            value: value,
-            recipient: recipient,
-            complete: false,
-            approvalCount: 0
-            });
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false,
+           approvalCount: 0
+        });
 
         requests.push(newRequest);
-
     }
 
     function approveRequest(uint index) public {
@@ -67,19 +66,31 @@ contract Campaign {
 
         request.approvals[msg.sender] = true;
         request.approvalCount++;
-
     }
 
     function finalizeRequest(uint index) public restricted {
         Request storage request = requests[index];
 
+        require(request.approvalCount > (approversCount / 2));
         require(!request.complete);
-        require(request.approvalCount > approversCount/2);
 
         request.recipient.transfer(request.value);
         request.complete = true;
-
     }
 
+    function getSummary() public view returns (
+      uint, uint, uint, uint, address
+      ) {
+        return (
+          minimumContribution,
+          this.balance,
+          requests.length,
+          approversCount,
+          manager
+        );
+    }
 
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
+    }
 }
